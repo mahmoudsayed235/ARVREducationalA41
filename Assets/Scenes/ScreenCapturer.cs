fe3ee2196525bee152fbc556bfaebf2a2879761a
Mahmoud Elmadah <mahmoud.sayed235@gmail.com>
2020-11-30 04:56:24 +0200

    commit

---
+++ Assets/Scenes/ScreenCapturer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenCapturer : MonoBehaviour
+{
+    public string namePrefix;
+    public int counter = 0;
+
+	void Update ()
+    {
+        if(Input.GetKeyUp(KeyCode.C))
+        {
+            ScreenCapture.CaptureScreenshot(string.Format("{0}_{1}.png", namePrefix, counter++));
+        }
+	}
+}
